a7524b1ecb443bf0a112a39e875789b67f2e917a
Igor Rivin <rivin@temple.edu>
2019-04-29 15:00:58 -0400

    latest and greatest

--- runstrat.py
+++ runstrat.py
@@ -1,14 +1,21 @@
 from prostrat import *
 import argparse
 
-def main(args):
-    infile = args.infile
-    outfile = args.outfile
-    freq = int(args.freq)
-    portnum = int(args.portnum)
-    clientid = int(args.clientid)
+timedict = {'1 secs':'1s', '5 secs':'5s', '10 secs':'10s', '15 secs':'15s', '30 secs':'30s', '1 min': '60s', '2 mins': '120s', '3 mins':'180 s', '5 mins': '300s', '10 mins':'600s', '15 mins': '900s', '20 mins': '1200s', '30 mins': '1800s', '1 hour': '3600s', '2 hours': '7200s', '3 hours':'3H','4 hours':'4H', '8 hours': '8H', '1 day':'1D'}
+
+def main(args=None, infile=None, outfile=None, freq=None, portnum=None, clientid=None):
+    if args is not None:
+        infile = args.infile
+        outfile = args.outfile
+        freq = args.freq
+        portnum = int(args.portnum)
+        clientid = int(args.clientid)
+    
+    freq2 = timedict[freq]
+    
+    
     print('yay')
-    profinit(clientid, portnum)
+    profinit(clientid, socknum=portnum)
     
     thedf = pd.read_csv(infile, index_col=0)
     print('yay')
@@ -16,20 +23,21 @@ def main(args):
     preddict = {}
     for i in symbols:
         try:
-            preddict[i] = doprof(Stock(i, 'SMART', 'USD'))
+            preddict[i] = doprof(Future(i, '201906', 'GLOBEX'), freq=freq, freq2=freq2)
         except:
             pass
     preds = pd.Series(preddict)
-    thedf.set_index(columns[0])
-    newdf =pd.concat([thedf, preds], axis=1)
-    newdf.columns = thedf.columns.append('prediction')
+    predsdf = pd.DataFrame(preds, columns = ['prediction'])
+    print(thedf.head())
+    thedf.set_index(thedf.columns[0], inplace=True)
+    newdf =pd.concat([thedf, predsdf], axis=1)
     newdf = newdf[['cpair', 'prediction']]
+    print(newdf)
     newdf.to_csv(outfile)
+    return 0
     
     if __name__=="__main__":
         parser = argparse.ArgumentParser()
-        #parser.add_argument('fname', help="history file")
-        #parser.add_argument('tfile', help= "testfile")
         parser.add_argument('infile', help="input dataframe")
         parser.add_argument('outfile', help="output")
         parser.add_argument('freq', help='sampling frequency')
