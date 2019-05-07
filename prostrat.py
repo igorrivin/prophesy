13631297bc3d278df30bfad0b293b0bdddc0974c
Igor Rivin <rivin@temple.edu>
2019-05-07 18:23:55 -0400

    foo

--- prostrat.py
+++ prostrat.py
@@ -9,7 +9,7 @@ ib=IB()
 def profinit(idnum,  ipaddr='127.0.0.1', socknum=7497):
     ib.connect(ipaddr, socknum, clientId=idnum)
 
-def doprof(contract, hist='50 D', freq='1 hour', freq2='1H', lookahead=24):
+def doprof(contract, hist='50 D', freq='1 hour', freq2='1H', lookahead=24, include_history=True):
     bars = ib.reqHistoricalData(
         contract,
         endDateTime='',
@@ -24,7 +24,7 @@ def doprof(contract, hist='50 D', freq='1 hour', freq2='1H', lookahead=24):
     prodf.y = np.log(prodf.y)
     pp = Prophet(changepoint_prior_scale=0.001)
     pp.fit(prodf)
-    future = pp.make_future_dataframe(lookahead, freq=freq2)
+    future = pp.make_future_dataframe(periods=lookahead, freq=freq2, include_history=include_history)
     forecast = pp.predict(future)
     #return pd.concat([prodf, forecast], axis=1)
     return forecast.tail(1).yhat.iloc[0]-prodf.tail(1).y.iloc[0]
